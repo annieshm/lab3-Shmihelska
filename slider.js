f88d3301a851a70215316998e9608c725568f0c8
annieshm <133815696+annieshm@users.noreply.github.com>
2023-11-20 10:17:50 +0200

    Initial

---
+++ slider.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('#slider-container').slick({
+      infinite: true,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      speed: 1000,
+    });
+  });
+  
+  $(document).ready(function(){
+    $('#slider-container-two').slick({
+      infinite: true,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      speed: 1000,
+    });
+  });
+
+  $(document).ready(function(){
+    $('#slider-container-three').slick({
+      infinite: true,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      speed: 1000,
+    });
+  });
+  
